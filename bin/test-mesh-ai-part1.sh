c073418ae63f26daa1dc7475e19a14a530318d9a
kengres <kenguruka24@gmail.com>
2026-05-28 18:57:12 +0000

    0.0.1

--- bin/test-mesh-ai-part1.sh
+++ bin/test-mesh-ai-part1.sh
@@ -4,7 +4,7 @@ source "$SCRIPT_DIR/pre-run.sh"
 
 npm install -g newman --no-fund --no-audit --loglevel=error
 
-timeout 60 bash "$SCRIPT_DIR/newman.sh" "$SCRIPT_DIR/mesh_ai_tests_1.json" \
+timeout 60 bash "$SCRIPT_DIR/newman.sh" "$SCRIPT_DIR/collections/mesh_ai_tests_1.json" \
   --env-var "base_url=http://localhost:3000" \
   --env-var "chat_id=12345" \
   --env-var "document_id=12345"
